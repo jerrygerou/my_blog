afa6e70098157e1f2498b6f90404d7cbf520e733
Sara Gerou <saragerou@gmail.com>
2017-04-25 07:36:59 -0500

    Update everything to allow for image attachments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
 	validates_presence_of :title, :body
+	has_attached_file :image, styles: {med: "100x100",}
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
